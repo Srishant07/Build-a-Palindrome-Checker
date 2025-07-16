d4cdc0be5598f86eadc414bb6e5af07061d6e0b2
Desiti Srishant <desitisrishant@gmail.com>
2025-07-16 20:53:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-    const textInput = document.getElementById("text-input");
-    const checkBtn = document.getElementById("check-btn");
-    const result = document.getElementById("result");
+   const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
 
-    function isPalindrome(str) {
-      // Normalize string: remove non-alphanumerics and convert to lowercase
-      const cleaned = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
-      const reversed = cleaned.split("").reverse().join("");
-      return cleaned === reversed;
-    }
+function isPalindrome(str) {
+  // Normalize string: remove non-alphanumerics and convert to lowercase
+  const cleaned = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  const reversed = cleaned.split("").reverse().join("");
+  return cleaned === reversed;
+}
 
-    checkBtn.addEventListener("click", () => {
-      const input = textInput.value;
-      if (!input) {
-        alert("Please input a value.");
-        return;
-      }
+checkBtn.addEventListener("click", () => {
+  const input = textInput.value;
+  if (!input) {
+    alert("Please input a value.");
+    return;
+  }
 
-      const message = isPalindrome(input)
-        ? `${input} is a palindrome.`
-        : `${input} is not a palindrome.`;
+  const message = isPalindrome(input)
+    ? `${input} is a palindrome.`
+    : `${input} is not a palindrome.`;
 
-      result.textContent = message;
-    });
- 
+  result.textContent = message;
+});
